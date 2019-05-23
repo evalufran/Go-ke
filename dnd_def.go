f7c80ebd9d4a0355aef92dff407176562cff4f61
Bebbolus <lambis.net@gmail.com>
2019-05-23 13:01:41 +0000

    first commit

--- dnd_def.go
+++ dnd_def.go
@@ -1,39 +1,51 @@
 package main
-//importazione pacchetti
+
 import (
-	"html/template"
-	"net/http"
-	"log"
 	_"fmt"
+	"html/template"
+	"log"
+	"net/http"
 )
-//creazione di una struttura
+
 type Datas struct {
-	Name   string
-	Class	[]string
-	Razza	[]string
+	Nome   string
+	Genere string
+	Razza  string
 }
 
+
 func main() {
-	//lettura del file HTML, permette di riconoscere il template che deve essere abilitato nella porta :8080
-	tmpl := template.Must(template.ParseFiles("dnd.html"))
- // funzione che gestisce i dati, quando attivata invia i dati inseriti nel form alla struttura 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	tmpl1 := template.Must(template.ParseFiles("dnd.html"))
+	tmpl2 := template.Must(template.ParseFiles("answer.html"))
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
+			tmpl1.Execute(w, nil)
 			return
 		}
+
 		details := Datas{
-			Name:   r.FormValue("firstname"),
-			Class:	r.Form["classe"],
-			Razza:	r.Form["razza"],
+			Nome:   r.FormValue("firstname"),
+			Genere: r.FormValue("genere"),
+			Razza:  r.FormValue("razza"),
 		}
 
-		
 		// do something with details
-		log.Println(details.Name)
-//se la funzione ha inviato corrrettamente i dati ritorna true dovrebbe fare il redirect su altra pagina html
-		tmpl.Execute(w, struct{ Success bool }{true})
-	})
-//abilitazione della porta 8080
-	http.ListenAndServe(":8080", nil)
+		log.Println(details.Nome)
+		log.Println(details.Genere)
+		log.Println(details.Razza)
+		tmpl1.Execute(w, struct{ Success bool }{true})
+		
+		})
+
+		http.HandleFunc("/process", func(w http.ResponseWriter, r *http.Request) {
+			if r.Method != http.MethodPost {
+				tmpl2.Execute(w, nil)
+				return
+			}
+	
+			tmpl2.Execute(w, struct{ Success bool }{true})
+			
+			})
+	 http.ListenAndServe(":8080", nil)
+		
 }
