125bc941fee44db06bca5e9ed383597e0520a2e2
Bebbolus <lambis.net@gmail.com>
2019-05-23 09:50:27 +0000

    dnd

--- dnd_def.go
+++ dnd_def.go
@@ -1,12 +1,12 @@
 package main
-
+//importazione pacchetti
 import (
 	"html/template"
 	"net/http"
 	"log"
 	_"fmt"
 )
-
+//creazione di una struttura
 type Datas struct {
 	Name   string
 	Class	[]string
@@ -14,14 +14,14 @@ type Datas struct {
 }
 
 func main() {
+	//lettura del file HTML, permette di riconoscere il template che deve essere abilitato nella porta :8080
 	tmpl := template.Must(template.ParseFiles("dnd.html"))
-
+ // funzione che gestisce i dati, quando attivata invia i dati inseriti nel form alla struttura 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			tmpl.Execute(w, nil)
 			return
 		}
-
 		details := Datas{
 			Name:   r.FormValue("firstname"),
 			Class:	r.Form["classe"],
@@ -31,9 +31,9 @@ func main() {
 		
 		// do something with details
 		log.Println(details.Name)
-
+//se la funzione ha inviato corrrettamente i dati ritorna true dovrebbe fare il redirect su altra pagina html
 		tmpl.Execute(w, struct{ Success bool }{true})
 	})
-
+//abilitazione della porta 8080
 	http.ListenAndServe(":8080", nil)
 }
